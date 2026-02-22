212cd6130988b74b9d174f0d726641e12ca52930
Madison <adelstonem2@winthrop.edu>
2026-02-22 18:33:13 -0500

    Worked on Question5

--- Question5.java
+++ Question5.java
@@ -1,1 +1,27 @@
-// Copying and pasting C++ code from when already did the same prompt from CSCI207 converting to Java
+//NVM couldn't find it 
+
+import java.util.Scanner;
+
+Scanner input = new Scanner(System.in)
+String S = " ";
+String S' = " ";
+int SLength = 0;
+int Position = 0; 
+
+SLength = S.length();
+
+String[] array = new array[SLength];
+String[] newarray = new newarray[SLength];
+
+System.out.println("What is your string?");
+S = input.next();
+
+array = S.split('');
+if(Position == 0){
+
+	newarray[Position] = array[SLength + 1];
+	Position++;
+}
+ else {
+newarray[Position] = array[SLength - 1];
+}
