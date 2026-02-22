3eeff94aa75a5a9a0dfe7a00b16bfaa3fb67f4fb
Madison <adelstonem2@winthrop.edu>
2026-02-22 17:41:05 -0500

    Worked in array

--- Question3.java
+++ Question3.java
@@ -1,4 +1,6 @@
-import.util.Scanner;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class Question3{
 	public static void main(Strings[] args){
@@ -7,18 +9,19 @@ public class Question3{
 		int Position = 0;
 		int MaxValue = 0;
 
+
 		System.out.println("How many integers will you be inputting ?");
 
 		NumIn = input.nextInt();
 
-		int A[NumIn];
+		int A[] = new int[NumIn];
 
 		System.out.println("Start typing the values, only integers please. ");
 
 		int A[Position] = input.nextInt();
 		Position++;
 
-		MaxValue = A.max();
+		MaxValue = Arrays.asList().max();
 
 		System.out.println("The max value is : " + MaxValue);
 	}
