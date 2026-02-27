111032c82bcd74dad02af01f5c4baa59fadab8d9
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-26 22:26:41 -0500

    Added comments

--- Question4.java
+++ Question4.java
@@ -18,41 +18,42 @@ import java.util.Arrays;
 public class Question3{
         public static void main(String[] args){
                 
-		Scanner input = new Scanner(System.in);
-                int Count = 0;
-		long N = 1;
-		long D = 1;
+		Scanner input = new Scanner(System.in);//Scanner for in
+			
+        int Count = 0; //States the amount D is in N incriments
+		long N = 1; //Long number that we are checking if D is in
+		long D = 1; //Number that checking if is in N
 
-		int NumLeng = 0;
-                int ShareValue = 0;
+		int NumLeng = 0; //Getting the length of the number
+                int ShareValue = 0; 
                 int[] A = new int[NumIn];
 
 
-                System.out.println("Type your number now, please only one large whole number");
-                N = input.nextInt();
+                System.out.println("Type your number now, please only one large whole number"); //Asking for the whole bumber
+                N = input.next(); //Getting the input
+ 
+                System.out.println("Start typing the value to check, only a number from 0-9 please. "); //Geting the D to check
+				D	=	input.next();
 
-                System.out.println("Start typing the value to check, only a number from 0-9 please. ");
-		
 		int GetNum(D,NLeng){
 			D = input.nextInt();
 			NLeng = N.length + 1;
 			GetNum(D,NLeng);
 		}
 
-		int Number[] = new int[NLeng];
+		int Number[] = new int[NLeng];// creates array and makes space with the length of n 
 		
-		int Number[] = N;
+		int Number[] = N; //Puts in N
 
-
-		if (Chars.contains(Number,D))
+		if (Chars.contains(Number,D))  
 		{
 			Count++;
 		}
 
 
-                System.out.println("N = " + N);
-                System.out.println("D = " + D);
-                System.out.println("D appears in N " + Count + "times");
+                System.out.println("N = " + N); //Prints out N
+                System.out.println("D = " + D); ?//Prints out D
+                System.out.println("D appears in N " + Count + "times"); //Prints out the statement and how many times it is in it 
 
 
 
@@ -61,6 +62,7 @@ public class Question3{
 
         }
 }
+
 
 
 
