8135a217ad387e00d632eb3f1361f70e0950740f
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-26 23:05:52 -0500

    Should be done

--- Question4.java
+++ Question4.java
@@ -29,83 +29,80 @@
 * <Student's WID>
 *************************************************************************/
 
-
 import java.util.Scanner;
-import java.util.Arrays;
 
-public class Question3{
-        public static void main(String[] args){
-                
-		Scanner input = new Scanner(System.in);//Scanner for in
-			
-        int Count = 0; //States the amount D is in N incriments
-		long N = 1; //Long number that we are checking if D is in
-		long D = 1; //Number that checking if is in N
+public class Question3
+{
+        public static void main(String[] args)
+        {
+            Scanner input = new Scanner(System.in); //SCanner 
 
-		int NumLeng = 0; //Getting the length of the number
-                int ShareValue = 0; 
-                int[] A = new int[NumIn];
+            int sameNum = 0; //If the same increases 
+            int Count = 0;//Count incrimentor
+            long N = 0;//N as a String long
+            long D = 0;//D as a String long
+			String NString = " ";//N as a String 
+            String DString = " "; //D as a String 
 
+            System.out.println("Type your number now, please only one large whole number. Must be less than 9,223,372,036,854,775,807."); //Asks user for the long N number
+            N = input.nextLong();//Gets the N 
 
-                System.out.println("Type your number now, please only one large whole number"); //Asking for the whole bumber
-                N = input.next(); //Getting the input
- 
-                System.out.println("Start typing the value to check, only a number from 0-9 please. "); //Geting the D to check
-				D	=	input.next();
-/*****************************<Function Name>****************************
-* Description: a brief description of what the function does.
-*
-* Parameters: a list each parameter the function takes
-*
-* Pre: list the preconditions; i.e., the things that must be true
-* before the function is called. Usually these involve whether
-* and how parameters must be initialised before the call, etc.
-*
-* Post: List the postconditions; i.e., the things that are guaranteed
-* to be true when the function finishes, assuming that the
-* listed preconditions are satisfied.
-*
-* Returns: Describe what value the function returns, if any.
-*
-* Called by: list the name(s) of the function(s) that call this one.
-* Calls: list the name(s) of the function(s) that this one calls.
-************************************************************************/
-		int GetNum(D,NLeng){
-			D = input.nextInt();
-			NLeng = N.length + 1;
-			GetNum(D,NLeng);
-		}
+            NString = Long.toString(N); //Converts N long to N string
 
-		int Number[] = new int[NLeng];// creates array and makes space with the length of n 
-		
-		int Number[] = N; //Puts in N
+            System.out.println("Start typing the value to check, only a number from 0-9 please. "); //Asks user for the number
+            D = input.nextLong();// Takes in the long N number 
 
-		if (Chars.contains(Number,D))  
-		{
-			Count++;
-		}
+            DString = Long.toString(D); //Converst D ling to D string
 
+            valInNum(Count,NString,DString ,sameNum,N,D); //calls Value in Num function
 
-                System.out.println("N = " + N); //Prints out N
-                System.out.println("D = " + D); ?//Prints out D
-                System.out.println("D appears in N " + Count + "times"); //Prints out the statement and how many times it is in it 
-
-
-
-
-		
+            input.close();//closes scanner
 
         }
+            /*****************************<Function Name>****************************
+* Description:This compares the N string and D string to see if the letter in the position is the same if so increases same let
+*
+* Parameters: int Count,String NString,String DString,int sameNum,long N, long D
+*
+* Pre: Count must have a value the N and D must be changed to a string there must be a sameNum and it must initalize be set to zero same with count there must be a long N and D
+*
+* Post: That the all of the num in N will have been checked and the same Num would havev been set and count will be the length og n 
+*
+* Returns: Returns eiteher an error message or  System.out.println("N = " + N);
+            System.out.println("D = " + D);
+            System.out.println("D appears in N " + sameNum + " times");
+
+*
+* Called by: main,valInNum
+* Calls: calls valInNum
+************************************************************************/
+    public static void valInNum(int Count,String NString,String DString,int sameNum,long N, long D)
+    {
+        if (Count <= NString.length()-1) // if count is less than the length of N 
+        {
+            if (DString.charAt(0) == NString.charAt(Count)) //If D string letter is equal to the num of N 
+            {
+                sameNum++; //samenum increased
+                Count++;//count increased 
+                valInNum(Count,NString,DString ,sameNum,N,D);
+            }
+
+            else if (DString.charAt(0) != NString.charAt(Count))//If D string letter is not equal to the num of N 
+            {
+                Count++; //Count is increased 
+                valInNum(Count,NString,DString ,sameNum,N,D); //function called again
+            }
+        }
+
+        else if (Count == NString.length()) // if count is equal to the length of the N string then print 
+        {
+            System.out.println("N = " + N); // Outputs the D N the the same Num
+            System.out.println("D = " + D);
+            System.out.println("D appears in N " + sameNum + " times");
+        }
+        else 
+        {
+            System.out.println("Something went wrong in ValinNUm"); //Error message
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
