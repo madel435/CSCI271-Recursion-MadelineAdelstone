57868b381e285516925c2c51d61f8738cbe82fbf
madel435 <adelstonem2@mailbox.winthrop.edu>
2026-02-26 22:34:50 -0500

    Added name

--- Question6.java
+++ Question6.java
@@ -13,8 +13,24 @@ Write a JAVA program that reads a list of integers from the keyboard and stores 
 *must be recursive and should not print anything to the screen, the main program will! Solutions
 *to this question using a loop are not acceptable! In addition, calculate the running time of this
 *function and show your work in details.
+*******************************************************************
+* I declare and confirm the following:
+* - I have not discussed this program code with anyone other than my
+* instructor or the teaching assistants assigned to this course.
+* - I have not used programming code obtained from someone else,
+* or any unauthorised sources, including the Internet, either
+* modified or unmodified.
+* - If any source code or documentation used in my program was
+* obtained from other sources, like a text book or course notes,
+* I have clearly indicated that with a proper citation in the
+* comments of my program.
+* - I have not designed this program in such a way as to defeat or
+* interfere with the normal operation of the supplied grading code.
 *
-*************************************************************************/
+* <MAdeline Adelstone
+* <Student's WID>
+************************************************************************/
+
 
 
 import java.util.Scanner;
@@ -47,6 +63,7 @@ public class Question3{
                 System.out.println("Sum is  : " + SumValue);
         }
 }
+
 
 
 
